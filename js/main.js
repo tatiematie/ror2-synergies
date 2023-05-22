4a720f1577c0680ce5547c6c5252e250b33694fc
Tate <tschron@my.gcu.edu>
2023-05-22 15:17:55 -0700

    synergy matching implemented

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const itemTitle = document.querySelector('#item-title'),
     itemType = document.querySelector('#item-type'),
     itemThumb = document.querySelector('#item-thumbnail img'),
     itemDesc = document.querySelector('#item-description'),
-    itemSelect = document.querySelector('#item-select')
+    itemSelect = document.querySelector('#item-select'),
+    synergyList = document.querySelector('#synergy-list')
 
 const readFile = async (filepath) => {
     const response = await fetch(filepath)
     return response.json()
 }
+
+let itemData
 
 const createButton = (item) => {
     const { name, rarity, type, id, description } = item
@@ -22,6 +25,7 @@ const createButton = (item) => {
 
     button.type = 'button'
     button.title = name
+    button.setAttribute('data-item-id', id)
     button.setAttribute('rarity', rarityLowerCase)
 
     img.src = src
@@ -32,26 +36,11 @@ const createButton = (item) => {
     li.appendChild(button)
     itemSelect.appendChild(li)
 
-    button.addEventListener('click', () => {
-        const isActive = button.getAttribute('active') === 'true'
-
-        if (!isActive) {
-            const activeButton = document.querySelector('#item-select button[active=true]')
-
-            if (activeButton) {
-                activeButton.removeAttribute('active')
-            }
-
-            button.setAttribute('active', 'true')
-            updateDisplay(item)
-        }
-    })
-
     return button
 }
 
 const loadButtons = async () => {
-    const itemData = await readFile('json/items.json')
+    itemData = await readFile('json/items.json') // Assign value to itemData
 
     itemData.forEach((item, index) => {
         const button = createButton(item)
@@ -59,6 +48,7 @@ const loadButtons = async () => {
         if (index === 0) {
             button.setAttribute('active', 'true')
             updateDisplay(item)
+            updateSynergyList(item)
         }
     })
 }
@@ -75,4 +65,56 @@ const updateDisplay = (item) => {
     itemDesc.children[1].innerHTML = description
 }
 
-loadButtons()
+const updateSynergyList = (item) => {
+    synergyList.innerHTML = ''
+
+    itemData.forEach((dataItem) => {
+        const excludeTag = Array.isArray(item.exclude) ? item.exclude : []
+        if (
+            (item.synergies && item.synergies.includes(dataItem.id)) ||
+            (Array.isArray(dataItem.tags) && dataItem.tags.some((tag) => item.synergies && item.synergies.includes(tag))) &&
+            !excludeTag.includes(dataItem.id)
+        ) {
+            const listItem = document.createElement('li')
+            listItem.className = 'item'
+            listItem.setAttribute('rarity', dataItem.rarity.toLowerCase())
+
+            const img = document.createElement('img')
+            img.src = `assets/img/items/${dataItem.id}.png`
+            listItem.appendChild(img)
+
+            synergyList.appendChild(listItem)
+        }
+    })
+}
+
+const handleButtonClick = (item, button) => {
+    const isActive = button.getAttribute('active') === 'true'
+
+    if (!isActive) {
+        const activeButton = document.querySelector('#item-select button[active=true]')
+
+        if (activeButton) {
+            activeButton.removeAttribute('active')
+        }
+
+        button.setAttribute('active', 'true')
+        updateDisplay(item)
+        updateSynergyList(item)
+    }
+}
+
+const attachButtonEventListeners = () => {
+    const buttons = itemSelect.querySelectorAll('button')
+    buttons.forEach((button) => {
+        const item = itemData.find((dataItem) => dataItem.id === button.getAttribute('data-item-id'))
+        button.addEventListener('click', () => handleButtonClick(item, button))
+    })
+}
+
+const initializePage = async () => {
+    await loadButtons()
+    attachButtonEventListeners()
+}
+
+initializePage()
